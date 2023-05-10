3772f82227da3ef5a478ec43ff0cd046115b41a6
voldo <filipecavoldo@outlook.com>
2023-05-10 12:05:39 -0300

    added fruit object files

---
+++ fruit.c
@@ -0,0 +1,4 @@
+#include "fruit.h"
+#include "engine.h"
+
+void fruit_spawn(int snake_x_pos, int snake_y_pos);
---
+++ fruit.h
@@ -0,0 +1,11 @@
+#ifndef FRUIT_H
+#define FRUIT_H
+
+struct fruit{
+    int x_pos;
+    int y_pos;
+};
+
+void fruit_spawn(int snake_x_pos, int snake_y_pos);
+
+#endif
