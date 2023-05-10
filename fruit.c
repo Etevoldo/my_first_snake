ddff644b9314ce6e8a604652a10c7f372c2affa7
voldo <filipecavoldo@outlook.com>
2023-05-10 12:40:35 -0300

    fruit location randomized

--- fruit.c
+++ fruit.c
@@ -1,4 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
 #include "fruit.h"
 #include "engine.h"
 
-void fruit_spawn(int snake_x_pos, int snake_y_pos);
+struct fruit fruit_initialize(int snake_x_pos, int snake_y_pos){
+    srand((unsigned) time (NULL));
+    struct fruit fruit;
+    //check if there'll be an off by one error later
+    fruit.x_pos = rand() % CANVAS_WIDTH;
+    fruit.y_pos = rand() % CANVAS_HEIGHT;
+    //todo: add a conditional to not spawn on the body
+    return fruit;
+}
+
+void fruit_respawn(struct fruit *fruit, int snake_x_pos, int snake_y_pos){
+    srand((unsigned) snake_x_pos + snake_y_pos);
+    //check if there'll be an off-by-one error later
+    fruit->x_pos = rand() % CANVAS_WIDTH;
+    fruit->y_pos = rand() % CANVAS_HEIGHT;
+    return;
+}
--- fruit.h
+++ fruit.h
@@ -1,11 +1,14 @@
 #ifndef FRUIT_H
 #define FRUIT_H
 
+#include "engine.h"
+
 struct fruit{
     int x_pos;
     int y_pos;
 };
 
-void fruit_spawn(int snake_x_pos, int snake_y_pos);
+struct fruit fruit_initialize(int snake_x_pos, int snake_y_pos);
+void fruit_respawn(struct fruit *fruit, int snake_x_pos, int snake_y_pos);
 
 #endif
